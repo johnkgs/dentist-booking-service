9fbcd22d49f5145fa22a1cf48a9462baa95a19de
johnkgs <johnkelvinsouza@hotmail.com>
2024-04-19 16:36:13 -0300

    feat(wrc): setup ui lib

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@repo/ui/button";
 export default function Page(): JSX.Element {
   return (
     <main>
-      <Button appName="web">Click me!</Button>
+      <Button>Click me!</Button>
     </main>
   );
 }
