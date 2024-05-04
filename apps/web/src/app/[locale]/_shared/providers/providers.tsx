e81fb172435a962b57cf7a5413ff863404c2d78b
johnkgs <johnkelvinsouza@hotmail.com>
2024-05-04 19:10:08 -0300

    refactor(web): replace style to css

--- apps/web/src/app/[locale]/_shared/providers/providers.tsx
+++ apps/web/src/app/[locale]/_shared/providers/providers.tsx
@@ -2,7 +2,6 @@ import { ptBR } from "@clerk/localizations"
 import { ClerkProvider } from "@clerk/nextjs"
 
 import { I18nProviderClient } from "@repo/translation/client"
-import { cn } from "@repo/ui/utils"
 
 interface Props {
   locale: string
@@ -19,11 +18,8 @@ export function Providers(props: React.PropsWithChildren<Props>) {
         signInFallbackRedirectUrl={redirectUrl}
         signUpFallbackRedirectUrl={redirectUrl}
         appearance={{
-          elements: {
-            formButtonPrimary: cn(
-              "bg-primary text-primary-foreground !shadow-none hover:bg-primary/90",
-              "h-10 px-4 py-2"
-            )
+          variables: {
+            colorPrimary: "hsl(221.2, 83.2%, 53.3%)"
           }
         }}
       >
